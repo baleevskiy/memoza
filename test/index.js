9baaba15918718257170bc17a558d32a6dcb496e
undercover <baleevskii@gmail.com>
2017-03-24 20:42:24 +1100

    Initial

--- test/index.js
+++ test/index.js
@@ -1,8 +1,31 @@
+const _ = require('lodash');
 const assert = require('assert');
+const sinon = require('sinon');
 const lib = require('../lib');
 
 describe('lib', () => {
+  let sandbox;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
   it('should', () => {
-    assert(lib());
+    const config = {};
+    const memoza = lib(config);
+
+    const f = memoza((a,b,c) => { return a+b+c;});
+    f();
+    f();
+    //
+    // const func = sandbox.spy();
+    // const memoizedFunc = memoza(func);
+    // _.times(10, memoizedFunc);
+    // func
+
   });
 });
