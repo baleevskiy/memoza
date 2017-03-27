85863be899624797233c9c3668ec7718b0fabf55
Grigory Baleevskiy <baleevskii@gmail.com>
2017-03-27 14:09:43 +1100

    Lint

--- test/cache.test.js
+++ test/cache.test.js
@@ -1,49 +1,39 @@
-const _ = require('lodash');
 const assert = require('assert');
 const fsp = require('fs-promise');
-const fs = require('fs');
 const Cache = require('../lib/cache');
 
-describe.only('cache', () => {
+describe('cache', () => {
   let writableDirname;
 
   before(() => {
-    writableDirname = __dirname+'/fixtures';
+    writableDirname = `${__dirname}/fixtures`;
     return fsp.writeFile(`${writableDirname}/test.json`, '{"test": 123.45"}');
   });
 
-  after(() => {
-    return fsp.emptyDir(writableDirname);
-  });
+  after(() => fsp.emptyDir(writableDirname));
 
-  it('should reject on bad dirname', () => {
-    return new Cache({path: __dirname+'/bad'})
+  it('should reject on bad dirname', () => new Cache({ path: `${__dirname}/bad` })
       .check()
       .catch((err) => {
         assert(err.message.includes('no such file or directory'));
-      })
-  });
+      }));
 
-  it('should resolve on good dirname', () => {
-    return new Cache({path: writableDirname }).check()
-  });
+  it('should resolve on good dirname', () => new Cache({ path: writableDirname }).check());
 
   it('should give proper cachePath', () => {
-    assert.equal(new Cache({path: writableDirname}).cachePath('test'), `${writableDirname}/test.json`);
+    assert.equal(new Cache({ path: writableDirname }).cachePath('test'), `${writableDirname}/test.json`);
   });
 
   it('should set and get', () => {
     const cacheKey = 'some_file';
-    const sampleData = {data: 'data', foo: [1, 2, 3]};
-    const cache = new Cache({path: writableDirname});
+    const sampleData = { data: 'data', foo: [1, 2, 3] };
+    const cache = new Cache({ path: writableDirname });
     return cache
       .set(cacheKey, sampleData)
-      .then(() => {
-        return cache.get(cacheKey)
-      })
+      .then(() => cache.get(cacheKey))
       .then((object) => {
         assert.deepEqual(object, sampleData);
         return fsp.remove(cache.cachePath(cacheKey));
-      })
+      });
   });
 });
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,3 @@
-const _ = require('lodash');
 const assert = require('assert');
 const fsp = require('fs-promise');
 const sinon = require('sinon');
@@ -7,7 +6,7 @@ const lib = require('../lib');
 
 describe('lib', () => {
   let sandbox;
-  const fixturesDirectory = __dirname+'/fixtures';
+  const fixturesDirectory = `${__dirname}/fixtures`;
 
   const memoza = lib({ path: fixturesDirectory });
 
@@ -19,23 +18,20 @@ describe('lib', () => {
     sandbox.restore();
   });
 
-  after(() => {
-    return fsp.emptyDir(fixturesDirectory)
-  });
+  after(() => fsp.emptyDir(fixturesDirectory));
 
   it('should call function', () => {
-
     const cacheKeyFilename = `${fixturesDirectory}/test2.json`;
     sandbox.stub(cache.prototype, 'cachePath').returns(cacheKeyFilename);
     const functionSpy = sandbox.spy();
     const promiseSpy = sandbox.spy();
 
-    const f = memoza((a,b,cb) => {
-      functionSpy(a,b);
-      setTimeout(() => cb(a-b,a,b), 20);
-      return new Promise(res => {
-        promiseSpy(a,b);
-        setTimeout(() => res(a+b), 30);
+    const wrappedFunction = memoza((aa, bb, cb) => {
+      functionSpy(aa, bb);
+      setTimeout(() => cb(aa - bb, aa, bb), 20);
+      return new Promise((res) => {
+        promiseSpy(aa, bb);
+        setTimeout(() => res(aa + bb), 30);
       });
     });
     let cbResolver;
@@ -43,24 +39,23 @@ describe('lib', () => {
       cbResolver = resolver;
     });
 
-    const funcPromise = f(5, 8, (diff, a, b) => {
-      assert.deepEqual([diff,a ,b ], [-3, 5, 8]);
+    const funcPromise = wrappedFunction(5, 8, (diff, aa, bb) => {
+      assert.deepEqual([diff, aa, bb], [-3, 5, 8]);
     }).then((sum) => {
       assert.equal(sum, 13);
-      //call the function again to check whether we do not call function anymore
-      return f(5, 8, (diff, a, b) => {
-        assert.deepEqual([diff,a ,b ], [-3, 5, 8]);
+      // call the function again to check whether we do not call function anymore
+      return wrappedFunction(5, 8, (diff, aa, bb) => {
+        assert.deepEqual([diff, aa, bb], [-3, 5, 8]);
         cbResolver();
-      }).then((sum) => {
-        assert.equal(sum, 13);
-      })
+      }).then((sum2) => {
+        assert.equal(sum2, 13);
+      });
     });
 
     return Promise.all([funcPromise, cbPromise]).then(() => {
       assert(functionSpy.calledOnce);
       assert(promiseSpy.calledOnce);
     });
-
   });
 
 
